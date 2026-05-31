21c3af102d577524ec8473414cc062781a486ca7
Milan-Bishwokarma <bishwakarmamilan58@gmail.com>
2026-05-31 16:24:08 +0545

    second

--- decorates.py
+++ decorates.py
@@ -11,3 +11,19 @@ def greet(fx):
     return mfx 
 
 greet(hello)()
+
+class Student:
+    
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def info(self):
+        print(f"{self.name} and {self.age}")
+
+s1=Student("Milan",20)
+s1.info()
+
+
+
+   
